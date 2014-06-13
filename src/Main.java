30870cee2b8383a40d6d1476a248a6567474dc5c
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-06-13 23:53:10 +0200

    Add whitespace for clarity.

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ public class Main {
 		Expr x = new DoubleExpr();
 		Expr y = new DoubleExpr();
 		Expr e = new SubExpr(new AddExpr(x, y), y);
+
 		Expr[] arguments = { x, y };
 		Function f = new Function(arguments, e);
 		Expr v = f.apply(1.5, 2.5);
+
 		System.out.println(v.compile());
 	}
 

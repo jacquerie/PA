01c5c5f401bd3a651c8609f520476f66980707d3
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-06-21 02:39:48 +0200

    Clarify types of common operations in pdf.

--- tex/src/1/Expr.java
+++ tex/src/1/Expr.java
@@ -1,15 +1,15 @@
 public abstract class Expr {
 	public abstract Expr apply (Expr target, double value);
 
-	public Expr add (Expr that) {
+	public AddExpr add (Expr that) {
 		return new AddExpr(this, that);
 	}
 
-	public Expr mul (Expr that) {
+	public MulExpr mul (Expr that) {
 		return new MulExpr(this, that);
 	}
 
-	public Expr sub (Expr that) {
+	public SubExpr sub (Expr that) {
 		return new SubExpr(this, that);
 	}
 }
